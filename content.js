24a3d17bf093b922376201aa927044e82716e0ec
Andy Olsen <andy.olsen.tx@gmail.com>
2025-09-25 16:24:07 -0500

    check for unplayable video

--- content.js
+++ content.js
@@ -14,9 +14,41 @@ async function playNextVideo() {
         await browser.storage.local.set({ current });
         window.location.href = `https://www.youtube.com/watch?v=${queue[current].id}`;
         console.log("Play: ", queue[current].title);
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        if (!(await waitForPlayableVideo())) {
+            console.log("Video failed, skipping:", videoId);
+            // recursively call for next video
+            playNextVideo(queue, currentIndex + 1);
+        } else {
+            console.log("Video is playing:", videoId);
+        }
     } else {
         console.log("Queue is empty.");
     }
+}
+
+function waitForPlayableVideo(timeout = 5000) {
+    return new Promise((resolve) => {
+        const video = document.querySelector("video");
+        if (!video) return resolve(false);
+
+        let played = false;
+
+        // If the video starts playing, good
+        video.addEventListener(
+            "playing",
+            () => {
+                played = true;
+                resolve(true);
+            },
+            { once: true }
+        );
+
+        // If we time out before it plays, assume it's removed/private
+        setTimeout(() => {
+            if (!played) resolve(false);
+        }, timeout);
+    });
 }
 
 function attachListener() {
