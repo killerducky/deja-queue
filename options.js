94f4a23285d13252799b0a835aa6ebf3374f9d0c
Andy Olsen <andy.olsen.tx@gmail.com>
2025-09-30 19:52:52 -0500

    yet another color scheme

--- options.js
+++ options.js
@@ -28,27 +28,42 @@ function rating2color(rating) {
     // let colors = ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6"];
     // https://colorbrewer2.org/?type=qualitative&scheme=Set1&n=9
     // let colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628", "#f781bf", "#999999"];
+    // https://colorbrewer2.org/#type=diverging&scheme=RdBu&n=11
+    // const colors = ["#67001f", "#b2182b", "#d6604d", "#f4a582", "#fddbc7", "#f7f7f7", "#d1e5f0", "#92c5de", "#4393c3", "#2166ac", "#053061"];
+    const colors = [
+        "hsla(342, 100%, 20%, 1.00)",
+        "hsla(353, 76%, 40%, 1.00)",
+        "hsla(8, 63%, 57%, 1.00)",
+        "hsla(18, 84%, 68%, 1.00)",
+        "hsla(32, 65%, 75%, 1.00)",
+        "hsla(0, 0%, 70%, 1.00)",
+        "hsla(201, 50%, 70%, 1.00)",
+        "hsla(200, 63%, 60%, 1.00)",
+        "hsla(203, 66%, 47%, 1.00)",
+        "hsla(210, 70%, 36%, 1.00)",
+        "hsla(212, 90%, 20%, 1.00)",
+    ];
 
     // https://www.learnui.design/tools/data-color-picker.html
     // let colors = ["#003f5c", "#2f4b7c", "#665191", "#a05195", "#d45087", "#f95d6a", "#ff7c43", "#ffa600"].reverse();
 
     // https://www.vis4.net/palettes/#/9|s|7c316f,7be9ff,98cbff|ffffe0,ff005e,93003a|1|1
     // let colors = ["#7c316f", "#81477e", "#865b8d", "#896f9c", "#8c82ac", "#8d95bd", "#8ea8ce", "#8ebbe1", "#98cbff"];
-
-    const colors = [
-        "hsl(204, 90%, 11%)",
-        "hsl(219, 39%, 29%)",
-        "hsl(250, 27%, 41%)",
-        "hsl(306, 41%, 52%)",
-        "hsl(346, 63%, 64%)",
-        "hsl(20, 70%, 65%)",
-        "hsl(55, 80%, 45%)",
-        "hsl(85, 80%, 40%)",
-        "hsl(105, 80%, 45%)",
-    ].reverse();
+    // modify a bit:
+    // const colors = [
+    //     "hsl(204, 90%, 11%)",
+    //     "hsl(219, 39%, 29%)",
+    //     "hsl(250, 27%, 41%)",
+    //     "hsl(306, 41%, 52%)",
+    //     "hsl(346, 63%, 64%)",
+    //     "hsl(20, 70%, 65%)",
+    //     "hsl(55, 80%, 45%)",
+    //     "hsl(85, 80%, 40%)",
+    //     "hsl(105, 80%, 45%)",
+    // ].reverse();
 
     let colormap = {};
-    for (let i = 0, r = 9.0; i < colors.length; i++) {
+    for (let i = 0, r = 9.5; i < colors.length; i++) {
         colormap[r] = colors[i];
         r -= 0.5;
     }
