b60b45e6af19f8ca9c57b85bc275d12dc7ce97d3
Andy Olsen <andy.olsen.tx@gmail.com>
2025-10-04 15:39:28 -0500

    move from electron dir to main dir

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: __dirname + "/preload.js", // inject our bridge script
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  win.loadFile("index.html");
+  // win.loadURL("https://www.youtube.com");
+  win.webContents.openDevTools();
+
+  setTimeout(() => {
+    win.webContents.executeJavaScript("window.ytControl.pause()");
+  }, 5000);
+
+  // Get current time
+  win.webContents
+    .executeJavaScript("window.ytControl.getTime()")
+    .then((t) => console.log("Current time:", t));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
