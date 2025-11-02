2f6ad2b3c123e0bf3ee0453b30924a7d1c432fbf
Andy Olsen <andy.olsen.tx@gmail.com>
2025-11-02 12:19:06 -0600

    prod does *not* work.

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "electron-vite";
+import electron from "vite-plugin-electron";
 import path from "path";
 
 export default defineConfig({
+  plugins: [electron()],
   main: {
     entry: "src/main/index.js",
   },
@@ -10,10 +12,25 @@ export default defineConfig({
       index: path.join(__dirname, "src/preload/index.js"),
     },
   },
+  resolve: {
+    alias: {
+      // Alias the package name to the direct path of its ES module file
+      "tabulator-tables": path.resolve(
+        __dirname,
+        "node_modules/tabulator-tables/dist/js/tabulator_esm.min.js"
+      ),
+    },
+  },
   renderer: {
     root: "src/renderer",
     build: {
-      outDir: "../../dist/renderer",
+      outDir: "dist/renderer",
+      rollupOptions: {
+        external: [],
+      },
+      commonjsOptions: { include: [/node_modules/] },
     },
   },
+  optimizeDeps: { include: ["tabulator-tables"] },
+  //   ssr: { noExternal: ["tabulator-tables"] },
 });
